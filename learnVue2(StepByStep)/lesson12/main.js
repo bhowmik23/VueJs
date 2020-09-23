67e8c85bb6dbbee09edd1992db4c6535c22f91c4
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-09-23 18:57:45 +0600

    Component Communication Example #1: Custom Events

---
+++ learnVue2(StepByStep)/lesson12/main.js
@@ -0,0 +1,23 @@
+ Vue.component('coupon', {
+    template: `<input placeholder="Enter your coupon code" @blur="onCouponApplied">`,
+
+    methods: {
+        onCouponApplied() {
+            this.$emit('applied');
+        }
+    }
+})
+
+new Vue({
+    el: '#root',
+
+    data: {
+        couponApplied: false
+    },
+    methods: {
+        onCouponApplied() {
+            // alert(' It was Applied');
+            this.couponApplied = true;
+        }
+    },
+});
