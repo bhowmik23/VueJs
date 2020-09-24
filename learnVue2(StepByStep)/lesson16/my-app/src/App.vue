8259a646663976932446844224a185a5f127c59a
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-09-24 16:51:59 +0600

    Single-Use Components and Inline Templates

---
+++ learnVue2(StepByStep)/lesson16/my-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <message>Hello There</message>
+    <message>Hello There</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+
+export default {
+  name: 'app',
+  components: {Message},
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
