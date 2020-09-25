361315dadc7dbd68ee3c7f764e3da313a954d133
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-09-25 12:14:43 +0600

    Hot Module Replacement

---
+++ learnVue2(StepByStep)/lesson17/lesson/src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Count: {{ count}} </h2>
+        <p>
+            <button @click="count += 1">Increment</button>
+        </p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        };
+    },
+}
+</script>
